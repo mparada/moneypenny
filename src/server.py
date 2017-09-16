9e90ae5cdeec3e0033a44492e51d8c01913dcf7b
Jan Wilken Dörrie <jwdoerrie@gmail.com>
2017-09-16 20:37:18 +0200

    GET Transaction

--- src/server.py
+++ src/server.py
@@ -11,25 +11,41 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite://'
 db = SQLAlchemy(app)
 
+
 class Transaction(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     sender = db.Column(db.String(100))
-    receipient = db.Column(db.String(100))
+    recipient = db.Column(db.String(100))
     reference = db.Column(db.String(100))
     amount = db.Column(db.Integer)
     currency = db.Column(db.String(10))
     date = db.Column(db.DateTime())
 
-    def __init__(self, sender, receipient, reference, amount, currency):
+    def __init__(self, sender, recipient, reference, amount, currency):
         self.sender = sender
-        self.receipient = receipient
+        self.recipient = recipient
         self.reference = reference
         self.amount = amount
         self.currency = currency
-        self.date = datetime.datetime()
+        self.date = datetime.datetime.now()
+
+    def as_dict(self):
+        return {
+            "id": self.id,
+            "sender": self.sender,
+            "recipient": self.recipient,
+            "reference": self.reference,
+            "amount": self.amount,
+            "currency": self.currency,
+            "date": self.date.strftime('%d.%m.%Y')}
+
 
 @app.route('/transaction', methods=['GET', 'POST'])
 def transaction():
+    if request.method == 'GET':
+        transactions = Transaction.query.all()
+        return jsonify([t.as_dict() for t in transactions])
+
     if request.method == 'POST':
         data = request.get_json()
         login = None
@@ -39,19 +55,29 @@ def transaction():
                 break
 
         if not login:
-            return jsonify({ "speech": "Invalid Session"})
+            return jsonify({"speech": "Invalid Session"})
 
-        amount = login['parameters']['money']['amount']
-        currency = login['parameters']['money']['currency']
-        recipient = login['parameters']['recipient']
-        sender = login['parameters']['Name']
+        params = login['parameters']
+        amount = params['money']['amount']
+        currency = params['money']['currency']
+        recipient = params['recipient']
+        sender = params['Name']
+
+        transaction = Transaction(sender, recipient, "Money Penny Transfer",
+                                  amount, currency)
+
+        db.session.add(transaction)
+        db.session.commit()
+
         response = "Okay, sending {} {} from {} to {}".format(amount, currency,
-                sender, recipient)
-        return jsonify({ "speech": response, "displayText": response})
+                                                              sender, recipient)
+        return jsonify({"speech": response, "displayText": response})
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
 if __name__ == "__main__":
+    db.create_all()
     app.run(debug=True)
