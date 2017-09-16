a7ce54e802abcc1ac49c006fa5c105aabc266eed
Jan Wilken Dörrie <jwdoerrie@gmail.com>
2017-09-16 12:31:49 +0200

    Added App

---
+++ src/app/app.js
@@ -0,0 +1,41 @@
+/**
+ * Copyright 2017, Google, Inc.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+'use strict';
+
+// [START app]
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.status(200).send('Hello, world!').end();
+});
+
+
+app.get('/helloHttp', (req, res) => {
+  const response = "This is a sample response from your webhook!" //Default response from the webhook to show it's working
+
+  res.setHeader('Content-Type', 'application/json'); //Requires application/json MIME type
+  res.send(JSON.stringify({ "speech": response, "displayText": response}));
+});
+
+// Start the server
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
+  console.log('Press Ctrl+C to quit.');
+});
+// [END app]
